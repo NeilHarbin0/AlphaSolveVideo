b2b9a8c229f8a3e06a18970cc523178e9b1778cb
Neil Harbin <neilharbin0@gmail.com>
2024-02-11 12:29:32 -0500

    Added required gecko codes and some other helpful ones that I use

--- Setup/BuildRelease.bat
+++ Setup/BuildRelease.bat
@@ -1,9 +1,11 @@
 rem Copy files to a release directory
-rem Copy everything from scripts to bin
+rem Copy scripts
 xcopy "..\Scripts\*.bat" ".\bin\Release\Scripts" /c /i /y
-rem Copy example videos from resources to bin
+rem Copy example videos
 xcopy "..\Resources\*.avi" ".\bin\Release\Resources" /c /i /y
-rem Copy ffmpeg to bin
+rem Copy codes
+xcopy "..\Resources\*.txt" ".\bin\Release\Resources" /c /i /y
+rem Copy ffmpeg
 xcopy "..\packages\ffmpeg-6.0-full_build\bin\ffmpeg.exe" ".\bin\Release\" /c /y
 rem Copy license
 xcopy "..\LICENSE" ".\bin\Release\" /c /y
